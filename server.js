f76728a17312198f8fcbec9ec3a5581d604251e8
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-01-11 20:22:40 +0100

    27_5 1.0.0

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors')
+
+const PORT = 8080;
+
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.use(`/api`, testimonialsRoutes);
+app.use(`/api`, concertsRoutes);
+app.use(`/api`, seatsRoutes);
+
+
+app.use((req, res) => {
+    res.status(404).send('404 not found...');
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
